bf91fd84ca4e3755841d409694f92a04ece8dbcb
Erynia <47214662+Erynia@users.noreply.github.com>
2019-03-23 13:17:03 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@
 **/
 (function (pro){
 	pro.formatTime = function forMatTime(template) {
-		template = template || "{0}年{1}月{2}日 下午{3}时{4}分{5}秒";
+		template = template || "{0}年{1}月{2}日 {3}时{4}分{5}秒";
 		var matchAry = this.match(/\d+/g);
 		template = template.replace(/\{(\d+)\}/g,function (){
 			var n = arguments[1],
